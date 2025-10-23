206aa437d290b710b88b24158da0f73bcde7968f
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-10-23 13:10:30 +0200

    Aggiunto icona al progetto e alla form

--- Calculator/FormMain.Designer.cs
+++ Calculator/FormMain.Designer.cs
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // FormMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(320, 467);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.MaximizeBox = false;
-            this.Name = "FormMain";
-            this.Text = "Calcolatrice";
-            this.Load += new System.EventHandler(this.FormMain_Load);
-            this.ResumeLayout(false);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+			this.SuspendLayout();
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(320, 467);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "FormMain";
+			this.Text = "Calcolatrice";
+			this.Load += new System.EventHandler(this.FormMain_Load);
+			this.ResumeLayout(false);
 
         }
 
